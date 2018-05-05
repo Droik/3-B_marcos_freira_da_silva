cc62933481017b1681de5dfe35389ac416e04202
Marcos <Marcos@Marcos-PC>
2018-05-05 10:30:28 -0300

    criando tela aluno

--- marcos/src/br/ifro/edu/MenuPrincipalController.java
+++ marcos/src/br/ifro/edu/MenuPrincipalController.java
@@ -10,9 +10,13 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import  javafx.fxml.FXMLLoader ;
+import  javafx.stage.Stage ;
+import  java.io.IOException; 
 
 /**
  *
@@ -24,6 +28,8 @@ public class MenuPrincipalController implements Initializable {
     private TextField txtn1, txtn2, txtresult;
     @FXML
     private Button btnsoma;
+    @FXML
+    private Label label;
     @FXML
     private void soma (ActionEvent event) {
      Double num1 = Double.parseDouble(txtn1.getText());
@@ -37,7 +43,25 @@ public class MenuPrincipalController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }    
-    
+     @FXML
+    private void AbrirCadastroAluno (ActionEvent event) {
+            try{
+    FXMLLoader fxmlLoader = new FXMLLoader();
+    fxmlLoader.setLocation(getClass().getResource("Aluno.fxml"));
+    Scene scene = new Scene(fxmlLoader.load(),900,682);
+    Stage stage = new Stage();
+    stage.setTitle("Cadastrar Cliente");
+    stage.setScene(scene);
+    stage.show();
+}
+catch(IOException e){
+
+}
+    }
+    @FXML
+    private void FecharProjeto (ActionEvent event) {
+        
+    }
 }
